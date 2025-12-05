6799eb6a76822b8418c2a75e3ec7316602c81e27
TeiganPritchard <teigan.pritchard@dcmail.ca>
2025-12-04 20:55:03 -0500

    push

---
+++ Pokemon/Pokemon/Character.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemon
+{
+    public class Character
+    {
+        
+        public Character(string Name, int Atk, int Def, int HP) { 
+        
+        }
+    }
+}
--- Pokemon/Pokemon/Form1.cs
+++ Pokemon/Pokemon/Form1.cs
@@ -13,8 +13,6 @@ namespace Pokemon
     public partial class Form1 : Form
     {
         
-        
-
         public Form1()
         {
             InitializeComponent();
